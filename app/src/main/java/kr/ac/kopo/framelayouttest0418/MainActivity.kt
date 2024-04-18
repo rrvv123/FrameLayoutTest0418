3a1c9125d9be5bd127aa4a3269550fb84f8d589d
rrvv123 <eorms7391@gmail.com>
2024-04-18 14:27:02 +0900

    Frame Layout 예제 (5-19) 완성 - 0418 6교시

--- app/src/main/java/kr/ac/kopo/framelayouttest0418/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/framelayouttest0418/MainActivity.kt
@@ -1,20 +1,50 @@
 package kr.ac.kopo.framelayouttest0418
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn1 : Button
+    lateinit var btn2 : Button
+    lateinit var btn3 : Button
+    lateinit var imgv1 : ImageView
+    lateinit var imgv2 : ImageView
+    lateinit var imgv3 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btn1 = findViewById<Button>(R.id.btn1)
+        btn2 = findViewById<Button>(R.id.btn2)
+        btn3 = findViewById<Button>(R.id.btn3)
+
+        imgv1 = findViewById<ImageView>(R.id.imgv1)
+        imgv1 = findViewById<ImageView>(R.id.imgv2)
+        imgv1 = findViewById<ImageView>(R.id.imgv3)
+
+        imgv2.visibility = View.INVISIBLE
+        imgv3.visibility = View.INVISIBLE
+
+        btn1.setOnClickListener(btnListner)
+        btn2.setOnClickListener(btnListner)
+        btn3.setOnClickListener(btnListner)
+    }
+
+    val btnListner = OnClickListener{
+        imgv2.visibility = View.INVISIBLE
+        imgv3.visibility = View.INVISIBLE
+        when(it.id){
+            R.id.btn1 -> imgv1.visibility=View.VISIBLE
+            R.id.btn2 -> imgv2.visibility=View.VISIBLE
+            R.id.btn3 -> imgv3.visibility=View.VISIBLE
         }
     }
 }
